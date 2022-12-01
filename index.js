330cca7c040f04cc97ad49c2575c7288da2bbe55
Simon Siefke <simon.siefke@gmail.com>
2022-12-01 13:37:24 +0100

    fix code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const handleUncaughtExceptionMonitor = (error, origin) => {
   console.log("uncaught exception monitor called");
 };
 
-const main = () => {
+const main = async () => {
   process.on("uncaughtExceptionMonitor", handleUncaughtExceptionMonitor);
   throw new Error("oops");
 };
